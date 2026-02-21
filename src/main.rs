808b3d381d759e4d1c3baa79c7c707f86a4aa628
George Munyoro <george@munyoro.dev>
2026-02-21 23:53:56 +0200

    feat: bench util

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ mod uci;
 use uci::UCI;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
     let mut u = UCI::new();
+
+    if args.len() > 1 && args[1] == "bench" {
+        let tokens: Vec<&str> = args.iter().skip(1).map(|s| s.as_str()).collect();
+        u.bench(tokens);
+        return;
+    }
+
     u.uci_loop();
 }
